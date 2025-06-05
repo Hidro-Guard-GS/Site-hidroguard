20319fd9938546673296459e59b583e2370d4bfe
Vinicius <viniciuscavalcantidosreis@gmail.com>
2025-06-05 11:04:20 -0300

    definindo tema padrão

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ document.querySelectorAll('.theme-switcher').forEach(btn => {
       document.body.classList.add('theme-' + btn.dataset.theme);
     });
   });
+
+  // Definir tema padrão
+if (!document.body.classList.contains('theme-claro') &&
+!document.body.classList.contains('theme-nublado') &&
+!document.body.classList.contains('theme-chuva')) {
+document.body.classList.add('theme-claro');
+}
